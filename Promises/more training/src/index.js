4c255302c8b72b9b686b6b74c9e8f766df28ab6a
Barend Koorzen <BarendK@pragmaworld.net>
2020-03-12 04:27:42 +0200

    Update index.js

--- Promises/more training/src/index.js
+++ Promises/more training/src/index.js
@@ -1,12 +1,16 @@
 
-async function makePizza(){
-    setTimeout((resolve) => {
-        resolve('pizza in the making');        
-    }, 2000);
+function makePizza(){
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve('pizza in the making');        
+        }, 2000);
+    });
 }
 
+const pizza = makePizza();
 console.log(`StartingPizza`);
-await console.log(makePizza());
+makePizza
 console.log(`Pizza done`);
+
 
 
