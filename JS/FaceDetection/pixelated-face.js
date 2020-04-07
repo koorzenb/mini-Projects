840831a3362af23327991def593a437984cc19f8
Barend Koorzen <BarendK@pragmaworld.net>
2020-04-07 05:11:12 +0200

    wip

--- JS/FaceDetection/pixelated-face.js
+++ JS/FaceDetection/pixelated-face.js
@@ -1,10 +1,14 @@
-const video = document.querySelector('.webcam');
-const canvas = document.querySelector('.video');
-const ctx = canvas.getContext('2d');
-const faceCanvas = document.querySelector('.face');
-const faceCtx = faceCanvas.getContext('2d');
 const faceDetector = new window.FaceDetector();
+const video = document.querySelector('video.webcam');
+const canvas = document.querySelector('canvas.video');
+const ctx = canvas.getContext('2d');
+const faceCanvas = document.querySelector('canvas.face');
+const faceCtx = faceCanvas.getContext('2d');
+const SCALE = 1.2;
+const SIZE = 10;
 console.log(video, canvas, faceCanvas, faceDetector);
+
+up to 31min
 
 // const optionsInputs = document.querySelectorAll(
 //   '.controls input[type="range"]'
@@ -15,13 +19,30 @@ console.log(video, canvas, faceCanvas, faceDetector);
 //   SCALE: 1.35,
 // };
 
-async function populateVideo(){
+async function populateVideo() {
   const stream = await navigator.mediaDevices.getUserMedia({
-    video: {width: 1280, height: 720}
+    video: { width: 1280, height: 720 },
   });
   video.srcObject = stream;
   await video.play();
+  console.log(video.width);
+  
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  faceCanvas.width = video.videoWidth;
+  faceCanvas.height = video.videoHeight;
 }
 
-populateVideo();
-got up to 19 min
+async function detect() {
+  const faces = await faceDetector.detect(video);
+  faces.forEach(drawFace);
+  requestAnimationFrame(detect)
+}
+
+function drawFace(face) {
+  const {width, height, top, left } = face.boundingBox;
+  console.log({width, height, top, left });
+  
+}
+
+populateVideo().then(detect);
