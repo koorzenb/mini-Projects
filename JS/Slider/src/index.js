7eea0c241e6dde525267f23e81022a3d0f61bd00
BarendK <barend.koorzen@pragmaworld.net>
2020-04-19 07:44:30 +0200

    Working Slider

--- JS/Slider/src/index.js
+++ JS/Slider/src/index.js
@@ -4,12 +4,12 @@ function Slider(slider){
     }
 
     let current;
-    let prevent;
+    let prev;
     let next;
 
     const slides = slider.querySelector('.slides');
-    const prevButton = document.querySelector('.goToPrev');
-    const nextButton = document.querySelector('.goToNext');
+    const prevButton = slider.querySelector('.goToPrev');
+    const nextButton = slider.querySelector('.goToNext');
     
     function startSlider() {
         console.log();
@@ -18,23 +18,38 @@ function Slider(slider){
         prev = current.previousElementSibling || slides.lastElementChild;
         next = current.nextElementSibling || slides.firstElementChild;
         console.log({current, prev, next});
-        
     }
 
     function applyClasses() {
         current.classList.add("current");
-        prev.classList.add("prev");
+        prev.classList.add("prev");  
         next.classList.add("next");
+    }
+
+    function move(direction) {
+        const classesToRemove = ["prev","current","next"];
+        prev.classList.remove(...classesToRemove);
+        next.classList.remove(...classesToRemove);
+        current.classList.remove(...classesToRemove);
+
+        if (direction === "back") {
+            [prev, current, next] = [prev.previousElementSibling || slides.lastElementChild, prev, current];            
+        }
+        else {
+            [prev, current, next] = [current, next, next.nextElementSibling || slides.firstElementChild]
+        }
+
+        applyClasses();
     }
 
     startSlider();
     applyClasses();
+
+    prevButton.addEventListener('click', () => move("back"));
+    nextButton.addEventListener('click', move);
 }
 
 const mySlider = Slider(document.querySelector('.slider'));
 const dogSlider = Slider(document.querySelector('.dog-slider'));
-
-// console.log('Check at end of training if "Slider" can effectively be renamed to something else');
-// console.log('What is the property you use to select next sibling? Write syntax');
 
 
