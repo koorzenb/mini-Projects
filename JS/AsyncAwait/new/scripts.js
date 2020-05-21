d9aec6e3967ed4a983a530ebc9f600f3b749475e
Barend Koorzen <koorzenb@gmail.com>
2020-05-21 05:08:30 +0200

    wip

--- JS/AsyncAwait/new/scripts.js
+++ JS/AsyncAwait/new/scripts.js
@@ -1,15 +1,63 @@
-function ask(options) {
-    return new Promise(resolve => {
-        const popup = document.createElement('form');
-        popup.classList.add("popup");
-        popup.insertAdjacentHTML('afterbegin', `
-            <fieldset>
-                <label>${options.title}</label>
-            </fieldset>    
-        `);
-        console.log(popup);
-    })
+function wait(ms = 0) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+async function destroyPopup(popup) {
+  popup.classList.remove('open');
+  await wait(1000);
+  popup = null;
 }
+  
+  function ask(options) {
+    return new Promise(async function(resolve) {
+      // First we need to create a popup with all the fields in it
+      const popup = document.createElement('form');
+      popup.classList.add('popup');
+      popup.insertAdjacentHTML(
+        'afterbegin',
+        `<fieldset>
+          <label>${options.title}</label>
+          <input type="text" name="input"/>
+          <button type="submit">Submit</button>
+        </fieldset>`
+      );
 
-got ot 7:33
+      popup.addEventListener("submit", (e) => {
+          e.preventDefault;
+          resolve(e.target.input.value);          
+      }, {once: true});
+  
+      // check if they want a cancel button
+      if (options.cancel) {
+        const skipButton = document.createElement('button');
+        skipButton.type = 'button';
+        skipButton.textContent = 'Cancel'; 
+        popup.firstElementChild.appendChild(skipButton);
+        destroyPopup(popup)
+      }
+      
+      document.body.appendChild(popup);
+  
+      await wait(50);
+      popup.classList.add('open');
+    });
+  }
 
+
+  got to 31:40 
+
+  function askQuestion(e) {
+    const button = e.currentTarget;
+    const cancel = button.hasAttribute('data-cancel');
+      
+    const answer = await ask({ 
+      title: button.dataset.question,
+      cancel
+    });
+    
+  }
+
+  const buttons = document.querySelectorAll('[data-question]');
+  buttons.forEach(button => {button.addEventListener('click', askQuestion)});
+  
+  
