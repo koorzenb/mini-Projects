90fd240fc6e555f9e6616d44fb78d09bdc33bb70
BarendK <barend.koorzen@pragmaworld.net>
2020-07-15 07:28:57 +0200

    Speech - initial

---
+++ JS/Speech Recognition/readme.md
@@ -0,0 +1,8 @@
+# CRS Webcomponent Example
+
+## Introduction
+
+This small application's purpose is as a developer reference for people getting started with web components.  
+It only exposes core features of webcomponents, es6 and css.  
+The comments are there as a guide, but not to spoon feed.  
+It is your responsibility to ensure you understand and know how and when to use the relevant features.
---
+++ JS/Speech Recognition/speech.js
@@ -0,0 +1,17 @@
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+
+function start(params) {
+    if(!("SpeechRecognition" in window)) {
+        console.log("sorry, speechRecog not supported");
+        return;
+    }
+    const recog = new SpeechRecognition();
+    recog.continuous = true;
+    recog.interimResults = true;
+    recog.onresult = handleResult;
+    recog.start();
+    08:05;
+}
+
+start();
