cf79b8b60c8652f1130d7c12b289be77f20bc6cc
Barend Koorzen <koorzenb@gmail.com>
2020-07-23 04:34:07 +0200

    wip

--- JS/Speech Recognition/speech.js
+++ JS/Speech Recognition/speech.js
@@ -1,5 +1,6 @@
+import { handleResult } from "./handlers";
+ 
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-
 
 function start(params) {
     if(!("SpeechRecognition" in window)) {
@@ -11,7 +12,7 @@ function start(params) {
     recog.interimResults = true;
     recog.onresult = handleResult;
     recog.start();
-    08:05;
+    08:52
 }
 
 start();
